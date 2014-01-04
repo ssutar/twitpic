6b8d6fc2dee378f3fc9d133517a98ed27e8424b8
Santosh Sutar <santosh@Santosh-MacBook-Air.local>
2014-01-05 02:03:27 +0530

    Remove cookie plugin

--- public/javascripts/controllers/TimelineController.js
+++ public/javascripts/controllers/TimelineController.js
@@ -1,20 +1,6 @@
 twitPicApp.controller('TimelineController', function($scope, $rootScope, $location) {
     var auth = $rootScope.twitter;
     
-//    if (!auth && JSON.parse($.cookie('auth'))) {
-//        var options = {
-//            url: '/1.1/search/tweets.json?q=year&result_type=mixed&count=100&include_entities=true',
-//            method: 'GET',
-//            oauthio : {
-//                provider: 'twitter',
-//                tokens: JSON.parse($.cookie('auth')),
-//                request: {
-//                    url: 'https://api.twitter.com'
-//                }
-//            }
-//        };
-//    }
-    
     if (auth && auth.get) {
         auth.get('/1.1/search/tweets.json?q=year&result_type=mixed&count=100&include_entities=true')
             .done(function(data) {
